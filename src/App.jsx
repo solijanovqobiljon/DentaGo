672c960c56a51dc1bb62d37cd008b57ccaeb39d4
qobiljonsolijanov <qobiljonsolijanov@gmail.com>
2025-12-11 15:14:38 +0500

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import UserHome from "./Components/UserHome/userHome"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="user" element={<UserHome />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="user" element={<UserHome />} />
+    </Routes>
   );
 };
 
